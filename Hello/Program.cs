628ffdacc897c31d1230cdaefbea6a2f8be1caf3
Nikita Boyko <nikitabojko57@gmail.com>
2021-12-01 20:18:14 +0200

    It's commit

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,7 +6,7 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, beautiful World!");
         }
     }
 }
